4f2eb7c9faf995d04d7ae360544567ce8ea2d9af
hsetlik <haydensetlik@gmail.com>
2020-08-14 14:20:35 -0600

    initial commit

--- TopDownScroller/main.cpp
+++ TopDownScroller/main.cpp
@@ -1,15 +1,27 @@
 //
 //  main.cpp
-//  TopDownScroller
+//  SDL2_GameEngine
 //
 //  Created by Hayden on 8/14/20.
 //  Copyright © 2020 Hayden Setlik. All rights reserved.
 //
 
 #include <iostream>
+#include "GameGlass.hpp"
+
+Game *game= nullptr;
+const int windowWidth = 1200;
+const int windowHeight = 650;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    game = new Game();
+    //grid = new Grid();
+    game-> init("Title", 600, 600, windowWidth, windowHeight, false);
+    while(game->running()){
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+    game->clean();
     return 0;
 }
